c0fc0f0ce9aebd6082a519f8259785fa7a819c73
JulienR <rechenmann@gmail.com>
2019-11-07 23:12:41 +0100

    change android test to be more inclusive

--- app/src/androidTest/java/com/pixelnos/fire/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/pixelnos/fire/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.pixelnos.fire", appContext.getPackageName());
+        assertTrue(appContext.getPackageName().contains("com.pixelnos.fire"));
     }
 }

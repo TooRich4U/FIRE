7b2ca36293d20e378f9deed3213b9aac04200630
JulienR <rechenmann@gmail.com>
2019-11-08 23:34:31 +0100

    add currency

---
+++ app/src/test/java/com/pixelnos/fire/backend/expenseManager/CurrencyTest.java
@@ -0,0 +1,27 @@
+package com.pixelnos.fire.backend.expenseManager;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CurrencyTest {
+    @Test
+    public void createCurrency(){
+        String name = "US Dollars";
+        String shortName = "USD";
+        String symbol = "$";
+        int conversionRate = 1;
+        Currency currency = new Currency(name, shortName, symbol, conversionRate);
+        assertEquals(name, currency.getName());
+        assertEquals(shortName, currency.getShortName());
+        assertEquals(symbol, currency.getSymbol());
+        assertEquals(conversionRate, currency.getConversionRate(), 0.00001);
+    }
+
+    @Test
+    public void formatAmount(){
+        Currency currency = new Currency("US Dollars", "USD", "$", 1.0);
+        assertEquals("$100.25", currency.format(100.25));
+        assertEquals("$1'255.25", currency.format(1255.25001));
+    }
+
+}
